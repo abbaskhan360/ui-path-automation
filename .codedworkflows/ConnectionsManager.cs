22d4374d0dcd2c4ca4478dd2e558d392a9217559
Abbas Github 360 <abbas.khan@360factors.com>
2026-04-27 01:07:59 +0500

    Restructure project: move workflows to IssueManagementSingle and TaskManagement folders, fix Login.xaml selectors and navigation, fix ParentTicket.xaml URL from qa to stage, add GenerateHtmlReport.cs, add TaskMain.xaml for parallel execution

---
+++ .codedworkflows/ConnectionsManager.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+using System;
+
+namespace IssueManagement
+{
+    public class ConnectionsManager
+    {
+        public ConnectionsManager(ICodedWorkflowsServiceContainer resolver)
+        {
+        }
+    }
+}
